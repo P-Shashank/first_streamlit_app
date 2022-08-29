e55557b0e34aeefdc99ac56b2e9a83046e996e69
Ponaganti Shashank <112375788+P-Shashank@users.noreply.github.com>
2022-08-29 14:26:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,12 +4,12 @@ streamlit.header('Breakfast Favourites')
 streamlit.text('🥣 Omega3 & blueberry Oatmeal')
 streamlit.text('🥗 kale, Spinach & Rocket smoothie')
 streamlit.text('🐔 Hard-Boiled Free_Range Egg') 
-streamlit.text('🥑🍞 Avacado Toast')
+streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas
 my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index),['Avocado','Strawberries'])
 streamlit.dataframe(my_fruit_list)
 
