2d8f090488f61af6e7adb2a32883a2c57a0e4190
Ponaganti Shashank <112375788+P-Shashank@users.noreply.github.com>
2022-08-29 13:27:51 +0530

    Update streamlit_app.py
    added lines to the console

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega3 & blueberry Oatmeal')
+streamlit.text('kale, Spinach & Rocket smoothie')
+streamlit.text('Hard-Boiled Free_Range Egg') 
